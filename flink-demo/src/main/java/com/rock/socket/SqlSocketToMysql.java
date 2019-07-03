36093f7e775cfb5ab872c20a9b47451444a0025a
cuishilei <rockeycui@163.com>
2019-07-03 17:07:27 +0800

    demo first

--- flink-demo/src/main/java/com/rock/socket/SqlSocketToMysql.java
+++ flink-demo/src/main/java/com/rock/socket/SqlSocketToMysql.java
@@ -5,11 +5,13 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
@@ -19,85 +21,116 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class SqlSocketToMysql {
-	public static void main(String[] args) throws Exception {
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		StreamTableEnvironment tableEnv = StreamTableEnvironment.getTableEnvironment(env);
-
-		SocketSource source = new SocketSource("localhost", 9000, "\n", "UTF-8");
-
-		DataStreamSource<String> stringDataStreamSource = env.addSource(source);
-
-		TypeInformation[] typeInformations = new TypeInformation[]{Types.STRING, Types.STRING, Types.LONG};
-
-		DataStream<Row> map = stringDataStreamSource.map(new MapFunction<String, Row>() {
-			@Override
-			public Row map(String s) {
-				String[] split = s.split(",");
-				Row row = new Row(split.length + 1);
-				for (int i = 0; i < split.length; i++) {
-					row.setField(i, split[i]);
-				}
-				row.setField(split.length, System.currentTimeMillis());
-				return row;
-			}
-		}).returns(new RowTypeInfo(typeInformations));
-
-		tableEnv.registerDataStream("user1", map, "name,sex");
-
-		Table table = tableEnv.sqlQuery("select * from user1 where sex='1'");
-
-		DataStream<Row> dataStream = tableEnv.toAppendStream(table, Row.class);
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.getTableEnvironment(env);
 
 
-		DataStreamSink<Row> rowDataStreamSink = dataStream.addSink(new MySqlSink());
+        SocketSource source1 = new SocketSource("localhost", 9000, "\n", "UTF-8");
+        //SocketSource source2 = new SocketSource("localhost", 8000, "\n", "UTF-8");
 
-		env.execute("SqlSocketToMysql");
+        tableEnv.registerDataStream("call_record", getStream(env, source1), "phone,call,call_time.rowtime");
 
-	}
+        //查询5分钟内每个手机号的呼叫次数
+        String sql = "select phone,count(1) from call_record group by tumble(call_time,INTERVAL '5' SECOND), phone";
+        //
 
-	static class MySqlSink extends RichSinkFunction<Row> {
-		private static final long serialVersionUID = 1L;
-		private Connection conn = null;
 
-		@Override
-		public void open(Configuration parameters) throws Exception {
-			super.open(parameters);
-			super.open(parameters);
-			Class.forName("com.mysql.jdbc.Driver");
-			// 获取数据库连接
-			conn = DriverManager.getConnection("jdbc:mysql://39.107.224.153:3306/kangkang", "root", "wodeai1993");
-		}
+        Table table = tableEnv.sqlQuery(sql);
 
-		@Override
-		public void invoke(Row value, Context context) throws Exception {
-			PreparedStatement ps = null;
-			try {
-				ps = conn.prepareStatement("insert into test (name,sex) values (?,?)");
-				ps.setString(1, (String) value.getField(0));
-				ps.setString(2, (String) value.getField(1));
-				ps.executeUpdate();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (ps != null) {
-					ps.close();
-				}
-			}
-		}
+        DataStream<Row> rowDataStream = tableEnv.toAppendStream(table, Row.class);
 
-		@Override
-		public void close() throws Exception {
-			super.close();
-			try {
-				super.close();
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+        rowDataStream.print();
 
-	}
+        /*DataStreamSink<Row> rowDataStreamSink = dataStream.addSink(new MySqlSink());*/
+
+        env.execute("SqlSocketToMysql");
+
+    }
+
+    private static DataStream<Row> getStream(StreamExecutionEnvironment env, SocketSource socketSource) {
+        DataStreamSource<String> stringDataStreamSource = env.addSource(socketSource);
+
+        TypeInformation[] typeInformations = new TypeInformation[]{Types.STRING, Types.STRING, Types.LONG};
+
+        DataStream<Row> map = stringDataStreamSource.map(new MapFunction<String, Row>() {
+            @Override
+            public Row map(String s) {
+                String[] split = s.split(",");
+                Row row = new Row(split.length);
+                row.setField(0, split[0]);
+                row.setField(1, split[1]);
+                row.setField(2, Long.valueOf(split[2]));
+                return row;
+            }
+        }).returns(new RowTypeInfo(typeInformations));
+
+        // 事件时间
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        return map.assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<Row>() {
+            private long currentMaxTimestamp = 0L;
+
+            @Override
+            public long extractTimestamp(Row element, long previousElementTimestamp) {
+                long field = (long) element.getField(2);
+                try {
+                    currentMaxTimestamp = field;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return currentMaxTimestamp;
+            }
+
+            @Override
+            public Watermark getCurrentWatermark() {
+                return new Watermark(currentMaxTimestamp);
+            }
+        });
+    }
+
+    static class MySqlSink extends RichSinkFunction<Row> {
+        private static final long serialVersionUID = 1L;
+        private Connection conn = null;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            super.open(parameters);
+            Class.forName("com.mysql.jdbc.Driver");
+            // 获取数据库连接
+            conn = DriverManager.getConnection("jdbc:mysql://39.107.224.153:3306/kangkang", "root", "wodeai1993");
+        }
+
+        @Override
+        public void invoke(Row value, Context context) throws Exception {
+            PreparedStatement ps = null;
+            try {
+                ps = conn.prepareStatement("insert into test (sex,count) values (?,?)");
+                ps.setString(1, (String) value.getField(0));
+                ps.setInt(2, Integer.valueOf(String.valueOf(value.getField(1))));
+                ps.executeUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (ps != null) {
+                    ps.close();
+                }
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            super.close();
+            try {
+                super.close();
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
